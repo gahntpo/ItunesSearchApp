53c6b933afda4fe540554c1303b4957a5c523c96
Karin Prater <k.prater@tdsoftware.de>
2022-07-25 12:28:00 +0300

    Initial Commit

---
+++ ItunesSearchApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ItunesSearchApp
+//
+//  Created by Karin Prater on 25.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ItunesSearchApp/ItunesSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItunesSearchAppApp.swift
+//  ItunesSearchApp
+//
+//  Created by Karin Prater on 25.07.22.
+//
+
+import SwiftUI
+
+@main
+struct ItunesSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
